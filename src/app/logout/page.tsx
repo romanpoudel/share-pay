5407fb42478a39a9f274b142018f241541e6f660
Roman Poudel <52848888+romanpoudel@users.noreply.github.com>
2024-07-12 12:50:26 +0545

    logout

---
+++ src/app/logout/page.tsx
@@ -0,0 +1,17 @@
+import { signOut } from '@/auth';
+import React from 'react';
+
+const page = () => {
+  return (
+    <form
+      action={async (formData) => {
+        "use server";
+        await signOut({redirectTo: '/'});
+      }}
+    >
+      <button>logout</button>
+    </form>
+  );
+};
+
+export default page;
