3e99e50a60de3d5c961cf69b79ade7019fc70c31
Roman Poudel <romanpoudel325@gmail.com>
2024-08-02 13:27:33 +0545

    fix(profile): add1 test

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,6 +3,5 @@ import React from 'react';
 import PagePlaceholder from '@/components/page-placeholder';
 
 export default function ProfilePage() {
-  // asdfasdf
   return <PagePlaceholder pageName="Profile" />;
 }
